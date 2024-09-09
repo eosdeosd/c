615c6f63488b229be36e7ad25c7a1a2da8133747
huanzi989 <14123591+huanzi989@user.noreply.gitee.com>
2024-09-09 16:08:12 +0000

    update 老人专用源/libvio.js.
    Signed-off-by: huanzi989 <14123591+huanzi989@user.noreply.gitee.com>

--- 老人专用源/libvio.js
+++ 老人专用源/libvio.js
@@ -25,26 +25,26 @@ var rule = {
 		// 'User-Agent':'MOBILE_UA'
 		'User-Agent':'Mozilla/5.0 (Linux; Android 9; ASUS_Z01QD Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36'
 	},
-    class_parse:'.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;/(\\d+).html',
-	// class_parse:'.stui-header__menu li;a&&Text;a&&href;/.*_(\\d+).html',
+//    class_parse:'.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;/(\\d+).html',
+	 class_parse:'.stui-header__menu li;a&&Text;a&&href;/.*_(\\d+).html',
 	tab_exclude:'夸克网盘|百度云盘',
 	pagecount:{"27":1},
-//	lazy:`js: var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
-//var url = html.url;
-//var from = html.from;
-//var next = html.link_next;
-//var id = html.id;
-//var nid = html.nid;
-//  var paurl = request("https://libvio.cc/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
-//var paurl = request(HOST + "/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
-//if (/https/.test(paurl)) {
-//	var purl = paurl + url + "&next=" + next + "&id=" + id + "&nid=" + nid;
-//	input = {
-//		jx: 0,
-//		url: request(purl).match(/var .* = '(.*?)'/)[1],
-//		parse: 0
-//	}
-// }`,
+	lazy:`js: var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);
+var url = html.url;
+var from = html.from;
+var next = html.link_next;
+var id = html.id;
+var nid = html.nid;
+  var paurl = request("https://libvio.cc/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
+var paurl = request(HOST + "/static/player/" + from + ".js").match(/ src="(.*?)'/)[1];
+if (/https/.test(paurl)) {
+	var purl = paurl + url + "&next=" + next + "&id=" + id + "&nid=" + nid;
+	input = {
+		jx: 0,
+		url: request(purl).match(/var .* = '(.*?)'/)[1],
+		parse: 0
+	}
+ }`,
 	searchUrl:'/index.php/ajax/suggest?mid=1&wd=**&limit=50',
 	detailUrl:'/detail/fyid.html', //非必填,二级详情拼接链接
 	// detailUrl:'/detail_fyid.html', //非必填,二级详情拼接链接
